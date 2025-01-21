b5cee4ea8e33298f572ee7e2a324de6756dae736
Patrick Bozeman <pbozeman@gmail.com>
2025-01-21 08:59:18 -0800

    [svc] init project

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Verilog dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        # Python package with required dependencies
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        ]);
+
+        # Base build inputs common to all systems
+        baseBuildInputs = with pkgs; [
+          boolector
+          flock
+          gtkwave
+          icestorm
+          nextpnr
+          pythonEnv
+          sby
+          verilator
+          verilog
+          yices
+          yosys
+        ];
+
+        # Conditionally add packages if the system is not Darwin
+        buildInputs = baseBuildInputs ++
+          pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
+            # these packages don't work correctly on Darwin
+            pkgs.verible
+            pkgs.xdot
+          ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          inherit buildInputs;
+        };
+      }
+    );
+}
