53c21407f686abe21a83a8777ab439a5035d0497
Patrick Bozeman <pbozeman@gmail.com>
2025-01-27 11:20:56 -0800

    [mem_test][stripe] 4x stripe using 2x2 cards

--- rtl/mem_test_striped_ice40_sram/mem_test_striped_ice40_sram_top.sv
+++ rtl/mem_test_striped_ice40_sram/mem_test_striped_ice40_sram_top.sv
@@ -8,7 +8,7 @@
 // acceptance testing is TBD.
 
 module mem_test_striped_ice40_sram_top #(
-    parameter         NUM_S           = 2,
+    parameter         NUM_S           = 4,
     parameter integer SRAM_ADDR_WIDTH = 18,
     parameter integer SRAM_DATA_WIDTH = 8
 ) (
@@ -28,6 +28,18 @@ module mem_test_striped_ice40_sram_top #(
     output logic                       L_SRAM_256_B_WE_N,
     output logic [SRAM_ADDR_WIDTH-1:0] L_SRAM_256_B_ADDR_BUS,
     inout  wire  [SRAM_DATA_WIDTH-1:0] L_SRAM_256_B_DATA_BUS,
+
+    // SRAM C
+    output logic                       R_SRAM_256_A_OE_N,
+    output logic                       R_SRAM_256_A_WE_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] R_SRAM_256_A_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] R_SRAM_256_A_DATA_BUS,
+
+    // SRAM D
+    output logic                       R_SRAM_256_B_OE_N,
+    output logic                       R_SRAM_256_B_WE_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] R_SRAM_256_B_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] R_SRAM_256_B_DATA_BUS,
 
     // debug signals
     output logic [7:0] R_E,
@@ -67,11 +79,31 @@ module mem_test_striped_ice40_sram_top #(
       .debug1(R_F),
       .debug2(R_I),
 
-      .sram_io_addr({L_SRAM_256_B_ADDR_BUS, L_SRAM_256_A_ADDR_BUS}),
-      .sram_io_data({L_SRAM_256_B_DATA_BUS, L_SRAM_256_A_DATA_BUS}),
+      .sram_io_addr({
+        R_SRAM_256_B_ADDR_BUS,
+        R_SRAM_256_A_ADDR_BUS,
+        L_SRAM_256_B_ADDR_BUS,
+        L_SRAM_256_A_ADDR_BUS
+      }),
+      .sram_io_data({
+        R_SRAM_256_B_DATA_BUS,
+        R_SRAM_256_A_DATA_BUS,
+        L_SRAM_256_B_DATA_BUS,
+        L_SRAM_256_A_DATA_BUS
+      }),
       .sram_io_ce_n(),
-      .sram_io_we_n({L_SRAM_256_B_WE_N, L_SRAM_256_A_WE_N}),
-      .sram_io_oe_n({L_SRAM_256_B_OE_N, L_SRAM_256_A_OE_N})
+      .sram_io_we_n({
+        R_SRAM_256_B_WE_N,
+        R_SRAM_256_A_WE_N,
+        L_SRAM_256_B_WE_N,
+        L_SRAM_256_A_WE_N
+      }),
+      .sram_io_oe_n({
+        R_SRAM_256_B_OE_N,
+        R_SRAM_256_A_OE_N,
+        L_SRAM_256_B_OE_N,
+        L_SRAM_256_A_OE_N
+      })
   );
 
   always_ff @(posedge CLK) begin
