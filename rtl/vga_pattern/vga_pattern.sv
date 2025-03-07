31aa84dd31ce00194b5478584ce73c33a79ced22
Patrick Bozeman <pbozeman@gmail.com>
2025-03-07 09:08:49 -0800

    [svc] bump submodule

--- rtl/vga_pattern/vga_pattern.sv
+++ rtl/vga_pattern/vga_pattern.sv
@@ -32,6 +32,7 @@ module vga_pattern #(
     output logic                   vga_hsync,
     output logic                   vga_vsync
 );
+  localparam AW = 16;
   localparam CW = COLOR_WIDTH;
   localparam HW = 12;
   localparam VW = 12;
@@ -65,11 +66,18 @@ module vga_pattern #(
   logic [CW-1:0]         m_pix_red;
   logic [CW-1:0]         m_pix_grn;
   logic [CW-1:0]         m_pix_blu;
+  logic [HW-1:0]         m_pix_x;
+  logic [VW-1:0]         m_pix_y;
+  logic [AW-1:0]         m_pix_addr;
   logic                  m_pix_ready;
 
   // horizontal position
   logic [HW-1:0]         x;
   logic [HW-1:0]         x_next;
+
+  // vert position
+  logic [VW-1:0]         y;
+  logic [VW-1:0]         y_next;
 
   // the width of each column
   logic [HW-1:0]         col_width;
@@ -89,6 +97,9 @@ module vga_pattern #(
   logic [CW-1:0]         vga_pix_red;
   logic [CW-1:0]         vga_pix_grn;
   logic [CW-1:0]         vga_pix_blu;
+  logic [HW-1:0]         vga_pix_x;
+  logic [VW-1:0]         vga_pix_y;
+  logic [AW-1:0]         vga_pix_addr;
   logic                  vga_pix_ready;
   logic                  vga_error;
 
@@ -117,6 +128,8 @@ module vga_pattern #(
 
   always_comb begin
     x_next        = x;
+    y_next        = y;
+
     col_next      = col;
     col_edge_next = col_edge;
 
@@ -132,6 +145,12 @@ module vga_pattern #(
         x_next        = 0;
         col_next      = 0;
         col_edge_next = col_width;
+
+        if (y < v_visible - 1) begin
+          y_next = y + 1;
+        end else begin
+          y_next = 0;
+        end
       end
     end
   end
@@ -140,20 +159,29 @@ module vga_pattern #(
     if (!rst_n) begin
       m_pix_valid <= 1'b0;
       x           <= 0;
+      y           <= 0;
       col         <= 0;
       col_edge    <= col_width;
     end else begin
       m_pix_valid <= m_pix_valid_next;
       x           <= x_next;
+      y           <= y_next;
       col         <= col_next;
       col_edge    <= col_edge_next;
     end
   end
 
   assign {m_pix_red, m_pix_grn, m_pix_blu} = col_colors[col];
+  assign m_pix_x                           = x;
+  assign m_pix_y                           = y;
+
+  // TODO: it's ok, it isn't used.. but it should probably be removed from the
+  // pix interface
+  assign m_pix_addr                        = 0;
 
   svc_pix_cdc #(
-      .COLOR_WIDTH(COLOR_WIDTH)
+      .COLOR_WIDTH(COLOR_WIDTH),
+      .ADDR_WIDTH (AW)
   ) svc_pix_cdc_i (
       .s_clk  (clk),
       .s_rst_n(rst_n),
@@ -162,6 +190,10 @@ module vga_pattern #(
       .s_pix_red  (m_pix_red),
       .s_pix_grn  (m_pix_grn),
       .s_pix_blu  (m_pix_blu),
+      .s_pix_x    (m_pix_x),
+      .s_pix_y    (m_pix_y),
+      .s_pix_addr (m_pix_addr),
+
       .s_pix_ready(m_pix_ready),
 
       .m_clk      (pixel_clk),
@@ -170,13 +202,17 @@ module vga_pattern #(
       .m_pix_red  (vga_pix_red),
       .m_pix_grn  (vga_pix_grn),
       .m_pix_blu  (vga_pix_blu),
+      .m_pix_x    (vga_pix_x),
+      .m_pix_y    (vga_pix_y),
+      .m_pix_addr (vga_pix_addr),
       .m_pix_ready(vga_pix_ready)
   );
 
   svc_pix_vga #(
       .H_WIDTH    (HW),
       .V_WIDTH    (VW),
-      .COLOR_WIDTH(COLOR_WIDTH)
+      .COLOR_WIDTH(COLOR_WIDTH),
+      .ADDR_WIDTH (AW)
   ) svc_pix_vga_i (
       .clk  (pixel_clk),
       .rst_n(pixel_rst_n),
@@ -185,6 +221,9 @@ module vga_pattern #(
       .s_pix_red  (vga_pix_red),
       .s_pix_grn  (vga_pix_grn),
       .s_pix_blu  (vga_pix_blu),
+      .s_pix_x    (vga_pix_x),
+      .s_pix_y    (vga_pix_y),
+      .s_pix_addr (vga_pix_addr),
       .s_pix_ready(vga_pix_ready),
 
       .h_visible   (h_visible),
