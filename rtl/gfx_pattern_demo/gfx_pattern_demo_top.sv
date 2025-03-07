19d82f5cbe7335d6923bdeb5946dc0a566cde653
Patrick Bozeman <pbozeman@gmail.com>
2025-03-07 09:07:50 -0800

    [gfx] move demo to use L sram

--- rtl/gfx_pattern_demo/gfx_pattern_demo_top.sv
+++ rtl/gfx_pattern_demo/gfx_pattern_demo_top.sv
@@ -19,11 +19,11 @@ module gfx_pattern_demo_top #(
     output logic LED2,
 
     // sram
-    output logic [ SRAM_ADDR_WIDTH-1:0] R_SRAM_ADDR_BUS,
-    inout  wire  [SRAM_RDATA_WIDTH-1:0] R_SRAM_DATA_BUS,
-    output logic                        R_SRAM_CS_N,
-    output logic                        R_SRAM_OE_N,
-    output logic                        R_SRAM_WE_N,
+    output logic [ SRAM_ADDR_WIDTH-1:0] L_SRAM_ADDR_BUS,
+    inout  wire  [SRAM_RDATA_WIDTH-1:0] L_SRAM_DATA_BUS,
+    output logic                        L_SRAM_CS_N,
+    output logic                        L_SRAM_OE_N,
+    output logic                        L_SRAM_WE_N,
 
     // output vga to pmod e/f
     output logic [7:0] R_E,
@@ -67,11 +67,11 @@ module gfx_pattern_demo_top #(
 
       .continious_write(1'b1),
 
-      .sram_io_addr(R_SRAM_ADDR_BUS),
-      .sram_io_data(R_SRAM_DATA_BUS),
-      .sram_io_ce_n(R_SRAM_CS_N),
-      .sram_io_we_n(R_SRAM_WE_N),
-      .sram_io_oe_n(R_SRAM_OE_N),
+      .sram_io_addr(L_SRAM_ADDR_BUS),
+      .sram_io_data(L_SRAM_DATA_BUS),
+      .sram_io_ce_n(L_SRAM_CS_N),
+      .sram_io_we_n(L_SRAM_WE_N),
+      .sram_io_oe_n(L_SRAM_OE_N),
 
       .vga_red  (vga_red),
       .vga_grn  (vga_grn),
