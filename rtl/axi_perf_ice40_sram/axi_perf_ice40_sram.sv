3cd3675a23dc9be4d7e45f46f4dbcb8ac9fbabc9
Patrick Bozeman <pbozeman@gmail.com>
2025-03-26 09:42:01 -0700

    [axi_perf] add ice40 sram version of the perf test

---
+++ rtl/axi_perf_ice40_sram/axi_perf_ice40_sram.sv
@@ -0,0 +1,148 @@
+`ifndef AXI_PERF_ICE40_SRAM_SV
+`define AXI_PERF_ICE40_SRAM_SV
+
+`include "svc.sv"
+`include "svc_ice40_axi_sram.sv"
+
+`include "axi_perf.sv"
+
+module axi_perf_ice40_sram #(
+    parameter NAME            = "axi_ice40_sram",
+    parameter CLOCK_FREQ      = 100_000_000,
+    parameter BAUD_RATE       = 115_200,
+    parameter SRAM_ADDR_WIDTH = 20,
+    parameter SRAM_DATA_WIDTH = 16
+) (
+    input logic clk,
+    input logic rst_n,
+
+    output logic utx_pin,
+
+    // sram controller to io pins
+    output logic [SRAM_ADDR_WIDTH-1:0] sram_io_addr,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] sram_io_data,
+    output logic                       sram_io_we_n,
+    output logic                       sram_io_oe_n,
+    output logic                       sram_io_ce_n
+);
+  localparam AXI_ADDR_WIDTH = SRAM_ADDR_WIDTH + $clog2(SRAM_DATA_WIDTH / 8);
+  localparam AXI_DATA_WIDTH = SRAM_DATA_WIDTH;
+  localparam AXI_ID_WIDTH = 4;
+  localparam AXI_STRB_WIDTH = SRAM_DATA_WIDTH / 8;
+
+  logic                      m_axi_awvalid;
+  logic [AXI_ADDR_WIDTH-1:0] m_axi_awaddr;
+  logic [  AXI_ID_WIDTH-1:0] m_axi_awid;
+  logic [               7:0] m_axi_awlen;
+  logic [               2:0] m_axi_awsize;
+  logic [               1:0] m_axi_awburst;
+  logic                      m_axi_awready;
+  logic                      m_axi_wvalid;
+  logic [AXI_DATA_WIDTH-1:0] m_axi_wdata;
+  logic [AXI_STRB_WIDTH-1:0] m_axi_wstrb;
+  logic                      m_axi_wlast;
+  logic                      m_axi_wready;
+  logic                      m_axi_bvalid;
+  logic [  AXI_ID_WIDTH-1:0] m_axi_bid;
+  logic [               1:0] m_axi_bresp;
+  logic                      m_axi_bready;
+
+  // verilator lint_off: UNUSEDSIGNAL
+  logic                      m_axi_arvalid;
+  logic [  AXI_ID_WIDTH-1:0] m_axi_arid;
+  logic [AXI_ADDR_WIDTH-1:0] m_axi_araddr;
+  logic [               7:0] m_axi_arlen;
+  logic [               2:0] m_axi_arsize;
+  logic [               1:0] m_axi_arburst;
+  logic                      m_axi_arready;
+  logic                      m_axi_rvalid;
+  logic [  AXI_ID_WIDTH-1:0] m_axi_rid;
+  logic [AXI_DATA_WIDTH-1:0] m_axi_rdata;
+  logic [               1:0] m_axi_rresp;
+  logic                      m_axi_rlast;
+  logic                      m_axi_rready;
+  // verilator lint_on: UNUSEDSIGNAL
+
+  svc_ice40_axi_sram #(
+      .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
+      .AXI_DATA_WIDTH(AXI_DATA_WIDTH),
+      .AXI_ID_WIDTH  (AXI_ID_WIDTH)
+  ) svc_ice40_axi_sram_i (
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .s_axi_awvalid(m_axi_awvalid),
+      .s_axi_awaddr (m_axi_awaddr),
+      .s_axi_awid   (m_axi_awid),
+      .s_axi_awlen  (m_axi_awlen),
+      .s_axi_awsize (m_axi_awsize),
+      .s_axi_awburst(m_axi_awburst),
+      .s_axi_awready(m_axi_awready),
+      .s_axi_wdata  (m_axi_wdata),
+      .s_axi_wstrb  (m_axi_wstrb),
+      .s_axi_wlast  (m_axi_wlast),
+      .s_axi_wvalid (m_axi_wvalid),
+      .s_axi_wready (m_axi_wready),
+      .s_axi_bresp  (m_axi_bresp),
+      .s_axi_bid    (m_axi_bid),
+      .s_axi_bvalid (m_axi_bvalid),
+      .s_axi_bready (m_axi_bready),
+      .s_axi_arvalid(m_axi_arvalid),
+      .s_axi_araddr (m_axi_araddr),
+      .s_axi_arid   (m_axi_arid),
+      .s_axi_arready(m_axi_arready),
+      .s_axi_arlen  (m_axi_arlen),
+      .s_axi_arsize (m_axi_arsize),
+      .s_axi_arburst(m_axi_arburst),
+      .s_axi_rvalid (m_axi_rvalid),
+      .s_axi_rid    (m_axi_rid),
+      .s_axi_rresp  (m_axi_rresp),
+      .s_axi_rlast  (m_axi_rlast),
+      .s_axi_rdata  (m_axi_rdata),
+      .s_axi_rready (m_axi_rready),
+      .sram_io_addr (sram_io_addr),
+      .sram_io_data (sram_io_data),
+      .sram_io_we_n (sram_io_we_n),
+      .sram_io_oe_n (sram_io_oe_n),
+      .sram_io_ce_n (sram_io_ce_n)
+  );
+
+  assign m_axi_arvalid = 1'b0;
+  assign m_axi_arid    = 0;
+  assign m_axi_araddr  = 0;
+  assign m_axi_arlen   = 0;
+  assign m_axi_arsize  = 0;
+  assign m_axi_arburst = 0;
+  assign m_axi_rready  = 1'b0;
+
+  axi_perf #(
+      .NAME          (NAME),
+      .CLOCK_FREQ    (CLOCK_FREQ),
+      .BAUD_RATE     (BAUD_RATE),
+      .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
+      .AXI_DATA_WIDTH(AXI_DATA_WIDTH),
+      .AXI_ID_WIDTH  (AXI_ID_WIDTH)
+  ) axi_perf_i (
+      .clk    (clk),
+      .rst_n  (rst_n),
+      .utx_pin(utx_pin),
+
+      .m_axi_awvalid(m_axi_awvalid),
+      .m_axi_awaddr (m_axi_awaddr),
+      .m_axi_awid   (m_axi_awid),
+      .m_axi_awlen  (m_axi_awlen),
+      .m_axi_awsize (m_axi_awsize),
+      .m_axi_awburst(m_axi_awburst),
+      .m_axi_awready(m_axi_awready),
+      .m_axi_wvalid (m_axi_wvalid),
+      .m_axi_wdata  (m_axi_wdata),
+      .m_axi_wstrb  (m_axi_wstrb),
+      .m_axi_wlast  (m_axi_wlast),
+      .m_axi_wready (m_axi_wready),
+      .m_axi_bvalid (m_axi_bvalid),
+      .m_axi_bid    (m_axi_bid),
+      .m_axi_bresp  (m_axi_bresp),
+      .m_axi_bready (m_axi_bready)
+  );
+
+endmodule
+`endif
---
+++ tb/axi_perf_ice40_sram_tb.sv
@@ -0,0 +1,60 @@
+`include "svc_unit.sv"
+`include "svc_model_sram.sv"
+
+`include "axi_perf_ice40_sram.sv"
+
+// verilator lint_off: UNUSEDSIGNAL
+module axi_perf_ice40_sram_tb;
+  localparam SRAM_ADDR_WIDTH = 20;
+  localparam SRAM_DATA_WIDTH = 16;
+
+  `TEST_CLK_NS(clk, 10);
+  `TEST_RST_N(clk, rst_n);
+
+  logic                       utx_pin;
+
+  logic [SRAM_ADDR_WIDTH-1:0] sram_io_addr;
+  wire  [SRAM_DATA_WIDTH-1:0] sram_io_data;
+  logic                       sram_io_we_n;
+  logic                       sram_io_oe_n;
+  logic                       sram_io_ce_n;
+
+  axi_perf_ice40_sram #(
+      .CLOCK_FREQ     (100),
+      .BAUD_RATE      (10),
+      .SRAM_ADDR_WIDTH(SRAM_ADDR_WIDTH),
+      .SRAM_DATA_WIDTH(SRAM_DATA_WIDTH)
+  ) uut (
+      .clk    (clk),
+      .rst_n  (rst_n),
+      .utx_pin(utx_pin),
+
+      .sram_io_addr(sram_io_addr),
+      .sram_io_data(sram_io_data),
+      .sram_io_we_n(sram_io_we_n),
+      .sram_io_oe_n(sram_io_oe_n),
+      .sram_io_ce_n(sram_io_ce_n)
+  );
+
+  svc_model_sram #(
+      .ADDR_WIDTH(SRAM_ADDR_WIDTH),
+      .DATA_WIDTH(SRAM_DATA_WIDTH)
+  ) svc_model_sram_i (
+      .rst_n  (rst_n),
+      .we_n   (sram_io_we_n),
+      .oe_n   (sram_io_oe_n),
+      .ce_n   (sram_io_ce_n),
+      .addr   (sram_io_addr),
+      .data_io(sram_io_data)
+  );
+
+  task automatic test_basic();
+    // This is only a very basic smoke test to make sure it compiles
+    // and we can look at wave forms
+    #100000;
+  endtask
+
+  `TEST_SUITE_BEGIN(axi_perf_ice40_sram_tb, 100000);
+  `TEST_CASE(test_basic);
+  `TEST_SUITE_END();
+endmodule
