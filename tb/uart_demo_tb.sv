9a386afce0d76a58dbc6be1b559dd159410f5961
Patrick Bozeman <pbozeman@gmail.com>
2025-03-30 00:05:31 -0700

    [uart_demo] add toupper echo server to demo

--- tb/uart_demo_tb.sv
+++ tb/uart_demo_tb.sv
@@ -1,3 +1,5 @@
+`define SVC_TB_PRINT
+
 `include "svc_unit.sv"
 
 `include "uart_demo.sv"
@@ -7,20 +9,24 @@ module uart_demo_tb;
   `TEST_CLK_NS(clk, 10);
   `TEST_RST_N(clk, rst_n);
 
+  logic urx_pin;
   logic utx_pin;
+
+  assign urx_pin = 1'b1;
 
   uart_demo #(
       .CLOCK_FREQ(100),
-      .BAUD_RATE (10)
+      .BAUD_RATE (100)
   ) uut (
       .clk    (clk),
       .rst_n  (rst_n),
+      .urx_pin(utx_pin),
       .utx_pin(utx_pin)
   );
 
   task automatic test_basic();
     // This is only a very basic smoke test and isn't testing the actual uart
-    #100000;
+    #1000000;
   endtask
 
   `TEST_SUITE_BEGIN(uart_demo_tb, 1000000);
