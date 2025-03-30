a24f287de9c02f44d858dc1faa851479b397707f
Patrick Bozeman <pbozeman@gmail.com>
2025-03-30 00:05:31 -0700

    [uart_demo] add toupper echo server to demo

--- rtl/uart_demo/uart_demo.sv
+++ rtl/uart_demo/uart_demo.sv
@@ -3,30 +3,61 @@
 
 `include "svc.sv"
 `include "svc_str_iter.sv"
+`include "svc_uart_rx.sv"
 `include "svc_uart_tx.sv"
 
 module uart_demo #(
     parameter CLOCK_FREQ = 100_000_000,
     parameter BAUD_RATE  = 115_200
 ) (
-    input  logic clk,
-    input  logic rst_n,
+    input logic clk,
+    input logic rst_n,
+
+    input  logic urx_pin,
     output logic utx_pin
 );
   localparam STR_MAX_LEN = 128;
   localparam MSG_WIDTH = 8 * STR_MAX_LEN;
 
-  logic                 utx_en;
-  logic [          7:0] utx_data;
-  logic                 utx_busy;
+  typedef enum {
+    STATE_IDLE,
+    STATE_HELLO,
+    STATE_HELLO_WAIT,
+    STATE_ECHO
+  } state_t;
 
-  logic                 str_valid;
-  logic [MSG_WIDTH-1:0] str_msg;
-  logic                 str_ready;
+  state_t                 state;
 
-  logic                 chr_valid;
-  logic [          7:0] chr_data;
-  logic                 chr_ready;
+  logic                   utx_en;
+  logic   [          7:0] utx_data;
+  logic                   utx_busy;
+
+  logic                   urx_valid;
+  logic   [          7:0] urx_data;
+
+  logic                   str_valid;
+  logic   [MSG_WIDTH-1:0] str_msg;
+  logic                   str_ready;
+
+  logic                   chr_valid;
+  logic   [          7:0] chr_data;
+  logic                   chr_ready;
+
+  logic                   echo_valid;
+  logic   [          7:0] echo_data;
+
+  svc_uart_rx #(
+      .CLOCK_FREQ(CLOCK_FREQ),
+      .BAUD_RATE (BAUD_RATE)
+  ) svc_uart_rx_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .urx_valid(urx_valid),
+      .urx_data (urx_data),
+
+      .urx_pin(urx_pin)
+  );
 
   svc_uart_tx #(
       .CLOCK_FREQ(CLOCK_FREQ),
@@ -57,36 +88,63 @@ module uart_demo #(
       .m_ready  (chr_ready)
   );
 
-  assign chr_ready = !utx_busy;
-  assign utx_en    = chr_valid && !utx_busy;
-  assign utx_data  = chr_data;
-
-  // This is a little janky and would be better with a state machine,
-  // but it's fine for a demo
-  logic first_done;
-  logic second_done;
-
   always_ff @(posedge clk) begin
     if (!rst_n) begin
-      second_done <= 1'b0;
-      first_done  <= 1'b0;
-      str_valid   <= 1'b0;
+      state      <= STATE_IDLE;
+      str_valid  <= 1'b0;
+      echo_valid <= 1'b0;
+      echo_data  <= 0;
     end else begin
-      if (!str_valid || str_ready) begin
-        if (!first_done) begin
-          first_done <= 1'b1;
-          str_valid  <= 1'b1;
-          str_msg    <= "First\r\n";
-        end else if (!second_done) begin
-          second_done <= 1'b1;
-          str_valid   <= 1'b1;
-          str_msg     <= "Second\r\n";
-        end else begin
-          str_valid <= 1'b0;
+      str_valid  <= str_valid && !str_ready;
+      echo_valid <= echo_valid && !utx_busy;
+
+      case (state)
+        STATE_IDLE: begin
+          state <= STATE_HELLO;
         end
-      end
+
+        STATE_HELLO: begin
+          str_valid <= 1'b1;
+          str_msg   <= "Hello - to upper:\r\n";
+          state     <= STATE_HELLO_WAIT;
+        end
+
+        STATE_HELLO_WAIT: begin
+          // looking at str_ready here would be broken under normal
+          // ready/valid handshake rules, because it wouldn't necessarily mean
+          // that all the bytes were sent, just that it's ready to receive
+          // more.. but with the current implementation, it's ok to do this.
+          // This is probably a sign that the str iter needs a different api
+          // that exposes when the iteration actually completed.
+          if (str_ready) begin
+            state <= STATE_ECHO;
+          end
+        end
+
+        STATE_ECHO: begin
+          if (urx_valid) begin
+            echo_valid <= 1'b1;
+
+            // to upper
+            // a = 61, z = 7a
+            // a - A = 8'h20;
+            if (urx_data >= 8'h61 && urx_data <= 8'h7A) begin
+              echo_data <= urx_data - 8'h20;
+            end else begin
+              echo_data <= urx_data;
+            end
+          end
+        end
+      endcase
+
     end
   end
+
+  // This is a janky mux between the hello string and the data we received and
+  // did a toupper on. It's fine for a demo.
+  assign chr_ready = !utx_busy;
+  assign utx_en    = (!utx_busy && (chr_valid || echo_valid));
+  assign utx_data  = (state != STATE_ECHO ? chr_data : echo_data);
 
 endmodule
 `endif
--- rtl/uart_demo/uart_demo_top.sv
+++ rtl/uart_demo/uart_demo_top.sv
@@ -7,6 +7,7 @@ module uart_demo_top (
     input  logic CLK,
     output logic LED1,
     output logic LED2,
+    input  logic UART_RX,
     output logic UART_TX
 );
   logic rst_n;
@@ -20,8 +21,8 @@ module uart_demo_top (
   uart_demo uart_demo_i (
       .clk    (CLK),
       .rst_n  (rst_n),
+      .urx_pin(UART_RX),
       .utx_pin(UART_TX)
-
   );
 
   assign LED1 = 1'b0;
