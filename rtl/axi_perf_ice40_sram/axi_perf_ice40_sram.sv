02cda9f9fb9b6e20b0fd32802d987a701613ed53
Patrick Bozeman <pbozeman@gmail.com>
2025-04-04 23:49:00 -0700

    [axi_perf] move ice40 sram to right side

--- rtl/axi_perf_ice40_sram/axi_perf_ice40_sram.sv
+++ rtl/axi_perf_ice40_sram/axi_perf_ice40_sram.sv
@@ -10,7 +10,8 @@ module axi_perf_ice40_sram #(
     parameter CLOCK_FREQ      = 100_000_000,
     parameter BAUD_RATE       = 115_200,
     parameter SRAM_ADDR_WIDTH = 20,
-    parameter SRAM_DATA_WIDTH = 16
+    parameter SRAM_DATA_WIDTH = 16,
+    parameter STAT_WIDTH      = 32
 ) (
     input logic clk,
     input logic rst_n,
@@ -119,7 +120,8 @@ module axi_perf_ice40_sram #(
       .BAUD_RATE     (BAUD_RATE),
       .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
       .AXI_DATA_WIDTH(AXI_DATA_WIDTH),
-      .AXI_ID_WIDTH  (AXI_ID_WIDTH)
+      .AXI_ID_WIDTH  (AXI_ID_WIDTH),
+      .STAT_WIDTH    (STAT_WIDTH)
   ) axi_perf_i (
       .clk  (clk),
       .rst_n(rst_n),
