1edb3b5007e2fb72dc487f3b3f061bea0617fc92
Patrick Bozeman <pbozeman@gmail.com>
2025-04-04 23:49:00 -0700

    [axi_perf] move ice40 sram to right side

--- rtl/axi_perf_ice40_sram/axi_perf_ice40_sram_top.sv
+++ rtl/axi_perf_ice40_sram/axi_perf_ice40_sram_top.sv
@@ -15,12 +15,16 @@ module axi_perf_ice40_sram_top #(
     input  logic UART_RX,
     output logic UART_TX,
 
-    output logic [SRAM_ADDR_WIDTH-1:0] L_SRAM_ADDR_BUS,
-    inout  wire  [SRAM_DATA_WIDTH-1:0] L_SRAM_DATA_BUS,
-    output logic                       L_SRAM_CS_N,
-    output logic                       L_SRAM_OE_N,
-    output logic                       L_SRAM_WE_N
+    output logic [SRAM_ADDR_WIDTH-1:0] R_SRAM_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] R_SRAM_DATA_BUS,
+    output logic                       R_SRAM_CS_N,
+    output logic                       R_SRAM_OE_N,
+    output logic                       R_SRAM_WE_N
 );
+  localparam CLOCK_FREQ = 100_000_000;
+  localparam BAUD_RATE = 115_200;
+  localparam STAT_WIDTH = 16;
+
   logic rst_n;
 
   svc_init svc_init_i (
@@ -29,18 +33,22 @@ module axi_perf_ice40_sram_top #(
       .rst_n(rst_n)
   );
 
-  axi_perf_ice40_sram axi_perf_ice40_sram_i (
+  axi_perf_ice40_sram #(
+      .CLOCK_FREQ(CLOCK_FREQ),
+      .BAUD_RATE (BAUD_RATE),
+      .STAT_WIDTH(STAT_WIDTH)
+  ) axi_perf_ice40_sram_i (
       .clk  (CLK),
       .rst_n(rst_n),
 
       .urx_pin(UART_RX),
       .utx_pin(UART_TX),
 
-      .sram_io_addr(L_SRAM_ADDR_BUS),
-      .sram_io_data(L_SRAM_DATA_BUS),
-      .sram_io_ce_n(L_SRAM_CS_N),
-      .sram_io_we_n(L_SRAM_WE_N),
-      .sram_io_oe_n(L_SRAM_OE_N)
+      .sram_io_addr(R_SRAM_ADDR_BUS),
+      .sram_io_data(R_SRAM_DATA_BUS),
+      .sram_io_ce_n(R_SRAM_CS_N),
+      .sram_io_we_n(R_SRAM_WE_N),
+      .sram_io_oe_n(R_SRAM_OE_N)
   );
 
   assign LED1 = 1'b0;
