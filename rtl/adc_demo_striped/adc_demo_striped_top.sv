c731265ef91ada443a216c6476d43bdab2081153
Patrick Bozeman <pbozeman@gmail.com>
2025-06-23 11:17:43 -0700

    [adc][lattice] update striped demo to 512 sram. add lattice

--- rtl/adc_demo_striped/adc_demo_striped_top.sv
+++ rtl/adc_demo_striped/adc_demo_striped_top.sv
@@ -12,7 +12,7 @@
 module adc_demo_striped_top #(
     parameter  NUM_S           = 2,
     localparam COLOR_WIDTH     = 4,
-    parameter  SRAM_ADDR_WIDTH = 18,
+    parameter  SRAM_ADDR_WIDTH = 19,
     parameter  SRAM_DATA_WIDTH = 16,
     parameter  ADC_DATA_WIDTH  = 10
 ) (
@@ -20,20 +20,20 @@ module adc_demo_striped_top #(
     output logic LED1,
 
     // sram L
-    output logic                       SRAM_256_C_OE_N,
-    output logic                       SRAM_256_C_WE_N,
-    output logic                       SRAM_256_C_UB_N,
-    output logic                       SRAM_256_C_LB_N,
-    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_256_C_ADDR_BUS,
-    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_256_C_DATA_BUS,
+    output logic                       SRAM_512_A_OE_N,
+    output logic                       SRAM_512_A_WE_N,
+    output logic                       SRAM_512_A_UB_N,
+    output logic                       SRAM_512_A_LB_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_512_A_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_512_A_DATA_BUS,
 
     // sram R
-    output logic                       SRAM_256_D_OE_N,
-    output logic                       SRAM_256_D_WE_N,
-    output logic                       SRAM_256_D_UB_N,
-    output logic                       SRAM_256_D_LB_N,
-    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_256_D_ADDR_BUS,
-    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_256_D_DATA_BUS,
+    output logic                       SRAM_512_B_OE_N,
+    output logic                       SRAM_512_B_WE_N,
+    output logic                       SRAM_512_B_UB_N,
+    output logic                       SRAM_512_B_LB_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_512_B_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_512_B_DATA_BUS,
 
     // ADC inputs
     input  logic [ADC_DATA_WIDTH-1:0] ADC_X,
@@ -108,11 +108,11 @@ module adc_demo_striped_top #(
       .adc_grn_io(ADC_GRN),
       .adc_blu_io(ADC_BLU),
 
-      .sram_io_addr({SRAM_256_C_ADDR_BUS, SRAM_256_D_ADDR_BUS}),
-      .sram_io_data({SRAM_256_C_DATA_BUS, SRAM_256_D_DATA_BUS}),
+      .sram_io_addr({SRAM_512_A_ADDR_BUS, SRAM_512_B_ADDR_BUS}),
+      .sram_io_data({SRAM_512_A_DATA_BUS, SRAM_512_B_DATA_BUS}),
       .sram_io_ce_n(),
-      .sram_io_we_n({SRAM_256_C_WE_N, SRAM_256_D_WE_N}),
-      .sram_io_oe_n({SRAM_256_C_OE_N, SRAM_256_D_OE_N}),
+      .sram_io_we_n({SRAM_512_A_WE_N, SRAM_512_B_WE_N}),
+      .sram_io_oe_n({SRAM_512_A_OE_N, SRAM_512_B_OE_N}),
 
       .vga_red  (vga_red),
       .vga_grn  (vga_grn),
@@ -147,9 +147,9 @@ module adc_demo_striped_top #(
   // assign R_I             = error_cnt[15:8];
   // assign R_H             = error_cnt[7:0];
 
-  assign SRAM_256_C_UB_N = 1'b0;
-  assign SRAM_256_C_LB_N = 1'b0;
-  assign SRAM_256_D_UB_N = 1'b0;
-  assign SRAM_256_D_LB_N = 1'b0;
+  assign SRAM_512_A_UB_N = 1'b0;
+  assign SRAM_512_A_LB_N = 1'b0;
+  assign SRAM_512_B_UB_N = 1'b0;
+  assign SRAM_512_B_LB_N = 1'b0;
 
 endmodule
