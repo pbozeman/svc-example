c4605c7849d6c04f397eaad0b3655d0d0b6c93b7
Patrick Bozeman <pbozeman@gmail.com>
2025-06-29 19:37:06 -0700

    [sram] move more demos to use the 512 chips

--- rtl/gfx_shapes_demo_striped/gfx_shapes_demo_striped_top.sv
+++ rtl/gfx_shapes_demo_striped/gfx_shapes_demo_striped_top.sv
@@ -11,25 +11,25 @@
 module gfx_shapes_demo_striped_top #(
     parameter  NUM_S           = 2,
     localparam COLOR_WIDTH     = 4,
-    parameter  SRAM_ADDR_WIDTH = 20,
+    parameter  SRAM_ADDR_WIDTH = 19,
     parameter  SRAM_DATA_WIDTH = 16
 ) (
     input  logic CLK,
     output logic LED1,
 
-    output logic                       SRAM_256_A_OE_N,
-    output logic                       SRAM_256_A_WE_N,
-    output logic                       SRAM_256_A_UB_N,
-    output logic                       SRAM_256_A_LB_N,
-    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_256_A_ADDR_BUS,
-    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_256_A_DATA_BUS,
+    output logic                       SRAM_512_A_OE_N,
+    output logic                       SRAM_512_A_WE_N,
+    output logic                       SRAM_512_A_UB_N,
+    output logic                       SRAM_512_A_LB_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_512_A_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_512_A_DATA_BUS,
 
-    output logic                       SRAM_256_C_OE_N,
-    output logic                       SRAM_256_C_WE_N,
-    output logic                       SRAM_256_C_UB_N,
-    output logic                       SRAM_256_C_LB_N,
-    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_256_C_ADDR_BUS,
-    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_256_C_DATA_BUS,
+    output logic                       SRAM_512_B_OE_N,
+    output logic                       SRAM_512_B_WE_N,
+    output logic                       SRAM_512_B_UB_N,
+    output logic                       SRAM_512_B_LB_N,
+    output logic [SRAM_ADDR_WIDTH-1:0] SRAM_512_B_ADDR_BUS,
+    inout  wire  [SRAM_DATA_WIDTH-1:0] SRAM_512_B_DATA_BUS,
 
     // output vga to pmod e/f
     output logic [7:0] PMOD_A,
@@ -73,11 +73,11 @@ module gfx_shapes_demo_striped_top #(
 
       .continious_write(1'b0),
 
-      .sram_io_addr({SRAM_256_C_ADDR_BUS, SRAM_256_A_ADDR_BUS}),
-      .sram_io_data({SRAM_256_C_DATA_BUS, SRAM_256_A_DATA_BUS}),
+      .sram_io_addr({SRAM_512_B_ADDR_BUS, SRAM_512_A_ADDR_BUS}),
+      .sram_io_data({SRAM_512_B_DATA_BUS, SRAM_512_A_DATA_BUS}),
       .sram_io_ce_n(),
-      .sram_io_we_n({SRAM_256_C_WE_N, SRAM_256_A_WE_N}),
-      .sram_io_oe_n({SRAM_256_C_OE_N, SRAM_256_A_OE_N}),
+      .sram_io_we_n({SRAM_512_B_WE_N, SRAM_512_A_WE_N}),
+      .sram_io_oe_n({SRAM_512_B_OE_N, SRAM_512_A_OE_N}),
 
       .vga_red  (vga_red),
       .vga_grn  (vga_grn),
@@ -109,10 +109,10 @@ module gfx_shapes_demo_striped_top #(
 
   assign LED1            = 1'b0;
 
-  assign SRAM_256_A_UB_N = 1'b0;
-  assign SRAM_256_A_LB_N = 1'b0;
-  assign SRAM_256_C_UB_N = 1'b0;
-  assign SRAM_256_C_LB_N = 1'b0;
+  assign SRAM_512_A_UB_N = 1'b0;
+  assign SRAM_512_A_LB_N = 1'b0;
+  assign SRAM_512_B_UB_N = 1'b0;
+  assign SRAM_512_B_LB_N = 1'b0;
 
   // assign R_I  = error_cnt[15:8];
   // assign R_H  = error_cnt[7:0];
