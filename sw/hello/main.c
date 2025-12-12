2132f0ec5387985a92fe83108f3a32748696d9b1
Patrick Bozeman <pbozeman@gmail.com>
2025-12-11 16:01:14 -0800

    [hw][hello] add freq to hello world

--- sw/hello/main.c
+++ sw/hello/main.c
@@ -1,7 +1,10 @@
-#include "stdint.h"
-#include "stdio.h"
+#include "libsvc/stdio.h"
+#include "libsvc/sys.h"
 
 int main(void) {
+  uint32_t freq = svc_clock_freq();
+
   printf("Hello World\n");
+  printf("Freq: %d\n", freq);
   return 0;
 }
