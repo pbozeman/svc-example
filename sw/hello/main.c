d4365826958d17de571b127ca48989a4f06cfa88
Patrick Bozeman <pbozeman@gmail.com>
2026-01-07 23:57:57 -0800

    [sw][hello] blink led in hello app

--- sw/hello/main.c
+++ sw/hello/main.c
@@ -1,10 +1,26 @@
 #include "libsvc/stdio.h"
 #include "libsvc/sys.h"
 
+#include "mmio.h"
+#include "util.h"
+
+#define LED_OFFSET 0x08
+
 int main(void) {
+  uint32_t led_state = 1;
   uint32_t freq = svc_clock_freq();
 
+  mmio_write(LED_OFFSET, led_state);
   printf("Hello World\n");
   printf("Freq: %d\n", freq);
+
+  while (1) {
+    mmio_write(LED_OFFSET, led_state);
+    led_state = ~led_state;
+
+    printf("Hello...\n");
+    svc_delay(freq / 8);
+  }
+
   return 0;
 }
